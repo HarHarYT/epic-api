d9a9a318ea82cf2b3dbdf5342219d9e9d61408d1
HarHarYT <116351710+HarHarYT@users.noreply.github.com>
2024-07-14 23:10:56 -0400

    Create authcode.py

---
+++ authcode.py
@@ -0,0 +1,29 @@
+import requests
+
+
+url = 'https://account-public-service-prod.ol.epicgames.com/account/api/oauth/token'
+
+
+headers = {
+    'content-type': 'application/x-www-form-urlencoded',
+    'Authorization': 'Basic MzQ0NmNkNzI2OTRjNGE0NDg1ZDgxYjc3YWRiYjIxNDE6OTIwOWQ0YTVlMjVhNDU3ZmI5YjA3NDg5ZDMxM2I0MWE='
+}
+
+
+auth = input("Enter your authcode: ")
+
+profile_data = {
+    'grant_type': 'authorization_code',
+    'code': auth, 
+}
+
+
+response = requests.post(url, headers=headers, data=profile_data)
+
+
+if response.status_code == 200:
+    print("Request successful!")
+    print(response.json())
+else:
+    print(f"Request failed with status code {response.status_code}")
+    print(response.text)
